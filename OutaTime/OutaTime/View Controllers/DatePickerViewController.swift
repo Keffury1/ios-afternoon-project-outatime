9de3598e5f5ca6b87588f13e8ce36760b8feb504
Robert Keffury <bobby.keffury@me.com>
2019-08-13 08:42:11 -0700

    Created the storyboard and the outlets for the project. Added a segue from initial view controller to detail view controller.

---
+++ OutaTime/OutaTime/View Controllers/DatePickerViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Bobby Keffury on 8/13/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+    }
+    @IBAction func doneTapped(_ sender: Any) {
+    }
+    
+    
+
+}
---
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Bobby Keffury on 8/13/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func travelBackTapped(_ sender: Any) {
+    }
+    
+    
+    
+
+}
